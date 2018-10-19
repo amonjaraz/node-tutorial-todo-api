8392bcbba5b63e11563528931a94099e4a552d3b
Alex <monjaraz.alexandro@gmail.com>
2018-10-19 07:14:12 -0500

    add PATCH /todos/api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -71,6 +72,31 @@ app.delete('/todos/:id', (req,res)=>{
         res.status(400).send();
      });
 });
+
+app.patch('/todos/:id', (req,res)=>{
+    var id= req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    var body= _.pick(req.body, ['text', 'completed']); // picks propties, if they exist, and adds them to body object.
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.statu(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+})
 
 app.listen(3000, ()=>{
     console.log(`Started on Port ${port}`);
